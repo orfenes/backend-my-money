6100713dedec0ded4e48ac9a84e8d3584626bccd
Rodrigo Castro <rfocastro@sp.r7.com>
2018-03-12 20:13:11 -0300

    corrigido validacao do banco

--- backend-my-money/src/api/billingCycle/billingCycle.js
+++ backend-my-money/src/api/billingCycle/billingCycle.js
@@ -3,19 +3,15 @@ const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, required: true }
-})
+  value: { type: Number, min: 0, required: true }
+});
 
 const debtSchema = new mongoose.Schema({
-  name:{ type: String, required: true },
-  value: { type: Number,  min: 0,  required: true },
-  status: { 
-    type: String, 
-    required: false, 
-    uppercase: true,
-    enum: ['PAGO', 'PENDENTE', 'AGENDADO']
-   }
-})
+  name: { type: String, required: true },
+  value: { type: Number, min: 0, required: true },
+  status: { type: String,  required: false, uppercase: true ,
+        enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
+});
 
 const billingCycleSchema = new mongoose.Schema({
   name:{ type: String, require: true },
--- backend-my-money/src/config/database.js
+++ backend-my-money/src/config/database.js
@@ -2,3 +2,5 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = 'O atributo {PATH} é obrigatorio'
